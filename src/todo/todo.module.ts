938a36a45556b20608ce4c8c55084e86d3e7b7e4
Ravish <ravish.goyal@mylofamily.com>
2024-01-03 11:23:21 +0530

    fix

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoList, TodoListSchema } from './schema/todo.schema';
+import { TodoListController } from './todo.controller';
+import { TodoListService } from './todo.service';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: TodoList.name, schema: TodoListSchema }])],
-    controllers: [],
-    providers: []
+  imports: [
+    MongooseModule.forFeature([
+      { name: TodoList.name, schema: TodoListSchema },
+    ]),
+  ],
+  controllers: [TodoListController],
+  providers: [TodoListService],
 })
 export class TodoModule {}
